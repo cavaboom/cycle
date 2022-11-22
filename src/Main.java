2310b005dd28f317f4dd17ced2bf9799e45cb9d5
dendariev <den.dariev@gmail.com>
2022-11-23 02:49:10 +0800

    Задача 2.1

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,10 @@ public class Main {
         for (int i = 10; i >= -10; i--) {
             System.out.println(i);
         }
+        // Задача 2.1
+        System.out.println("Задача 2.1");
+        for (int i = 1904; i < 2096 ; i = i +4) {
+            System.out.println(i+ " год является високосным");
+        }
     }
 }
