ba37515f999de0223ffa78404fef5e46749775f2
dendariev <den.dariev@gmail.com>
2022-11-23 02:34:00 +0800

    Задача 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача 1.1
+        System.out.println("Задача 1.1");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
